5c2f9e00f47faf226eb13620548ee6b62fe8a67a
Karina <147284870+karina105@users.noreply.github.com>
2024-05-20 22:15:58 -0600

    Update product-services.js

--- js/services/product-services.js
+++ js/services/product-services.js
@@ -1,15 +1,15 @@
-const apiUrl = "https://challenge-alura-geek-liart.vercel.app/"; //  URL de la API desplegada en Vercel
+const apiUrl = "https://fake-api-dusky-six.vercel.app/productos"; //  URL de tu API desplegada en Vercel
 
 // Funciones para la página web
 
 const productList = () => {
-    return fetch(`${apiUrl}/productos`)
+    return fetch(apiUrl)
         .then((res) => res.json())
         .catch((err) => console.log(err));
 };
 
 const createProducts = (nombre, precio, imagen) => {
-    return fetch(`${apiUrl}/productos`, {
+    return fetch(apiUrl, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -26,12 +26,17 @@ const createProducts = (nombre, precio, imagen) => {
 };
 
 const deleteProduct = (id) => {
-    return fetch(`${apiUrl}/productos/${id}`, {
+    return fetch(`${apiUrl}/${id}`, {
         method: "DELETE"
     })
         .then(res => res.json())
         .catch((err) => console.log(err));
 };
+export const servicesProducts = {
+    productList,
+    createProducts,
+    deleteProduct,
+}
 export const servicesProducts = {
     productList,
     createProducts,
